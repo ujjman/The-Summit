1eeef9ecf55bfaad7ab9733eda5f72c5adea4278
ujjman <ujjwalsini.um@gmail.com>
2019-12-25 21:04:38 +0530

    world module for Terasology

---
+++ src/main/java/org/terasology/ujjmanworld/TutorialWorldRasterizer.java
@@ -0,0 +1,47 @@
+/*
+ * Copyright 2019 MovingBlocks
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.terasology.ujjmanworld;
+
+import org.terasology.math.ChunkMath;
+import org.terasology.math.geom.Vector3i;
+import org.terasology.world.generation.facets.SurfaceHeightFacet;
+import org.terasology.registry.CoreRegistry;
+import org.terasology.world.block.Block;
+import org.terasology.world.block.BlockManager;
+import org.terasology.world.chunks.CoreChunk;
+import org.terasology.world.generation.Region;
+import org.terasology.world.generation.WorldRasterizer;
+
+public class TutorialWorldRasterizer implements WorldRasterizer {
+    private Block snow;
+
+    @Override
+    public void initialize() {
+        BlockManager blockManager = CoreRegistry.get(BlockManager.class);
+        snow = blockManager.getBlock("CoreBlocks:Snow");
+    }
+
+    @Override
+    public void generateChunk(CoreChunk chunk, Region chunkRegion) {
+        SurfaceHeightFacet surfaceHeightFacet = chunkRegion.getFacet(SurfaceHeightFacet.class);
+        for (Vector3i position : chunkRegion.getRegion()) {
+            float surfaceHeight = surfaceHeightFacet.getWorld(position.x, position.z);
+            if (position.y < surfaceHeight) {
+                chunk.setBlock(ChunkMath.calcBlockPos(position), snow); //setting our world with snow
+            }
+        }
+    }
+}
